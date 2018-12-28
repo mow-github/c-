28181ace5502f3795f9838d4c3fcd7bbe360f11e
Mats Wikmar <matwik@gmail.com>
2018-12-28 16:04:50 +0100

    section8 eur to dollar converter

--- udemy_beginner_course/first/section8__66_operators/section8__66_operators.cpp
+++ udemy_beginner_course/first/section8__66_operators/section8__66_operators.cpp
@@ -2,8 +2,44 @@
 #include "pch.h"
 #include <iostream>
 
+#include <limits>
+#include <climits>
+#include <cfloat>
+
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::dec;
+using std::hex;
+using std::hexfloat;
+using std::defaultfloat;
+using std::numeric_limits;
+using std::showbase;
+using std::streamsize;
+
+using std::vector;
+using std::string;
+
 int main()
 {
-    std::cout << "section 8 start \n"; 
+
+	const double usd_per_eur{1.19};
+	double euros{0.0};
+	double dollars{0.0};
+
+	cout << "Welcome to the EUR to USD converter" << endl;
+	cout << "Enter the value in EUR: " << endl;
+
+
+	cin >> euros;
+	dollars = euros * usd_per_eur;
+
+	cout << euros << " Euros is eq. to $" << dollars << endl;
+
+
+	return 0;
 }
 
