fc325433dd5c12c81717a24a5d4098e197ff9588
Mats Wikmar <matwik@gmail.com>
2018-12-30 12:50:23 +0100

    section 9 89 - sum of product pairs

---
+++ udemy_beginner_course/first/section9_89_sum_proudct_pairs/section9_89_sum_proudct_pairs.cpp
@@ -0,0 +1,31 @@
+
+
+#include "pch.h"
+#include <iostream>
+
+#include <limits>
+#include <climits>
+#include <cfloat>
+
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::dec;
+using std::hex;
+using std::hexfloat;
+using std::defaultfloat;
+using std::numeric_limits;
+using std::showbase;
+using std::streamsize;
+
+using std::vector;
+using std::string;
+
+int main()
+{
+    cout << "Nested Loops - Sum of Product of all pairs of vector elements \n"; 
+}
+
