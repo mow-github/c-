5e520a643a3ab7526b508aae5040768cf4235f9d
Mats Wikmar <matwik@gmail.com>
2018-12-30 19:27:41 +0100

    section9_90 challenge - collection of numbers menu

---
+++ udemy_beginner_course/first/section9_90_challenge_menu_collection/section9_90_challenge_menu_collection.cpp
@@ -0,0 +1,30 @@
+
+#include "pch.h"
+#include <iostream>
+
+#include <limits>
+#include <climits>
+#include <cfloat>
+
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::dec;
+using std::hex;
+using std::hexfloat;
+using std::defaultfloat;
+using std::numeric_limits;
+using std::showbase;
+using std::streamsize;
+
+using std::vector;
+using std::string;
+
+int main()
+{
+    std::cout << "Section 9 90 Challenge - collection of numbers \n"; 
+}
+
