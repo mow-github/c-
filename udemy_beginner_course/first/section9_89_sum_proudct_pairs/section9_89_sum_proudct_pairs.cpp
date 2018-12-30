6ce87a4ed7d411f8e352bcc74f2112c9f729b3d1
Mats Wikmar <matwik@gmail.com>
2018-12-30 19:16:24 +0100

    nested loop sum of vector pairs

--- udemy_beginner_course/first/section9_89_sum_proudct_pairs/section9_89_sum_proudct_pairs.cpp
+++ udemy_beginner_course/first/section9_89_sum_proudct_pairs/section9_89_sum_proudct_pairs.cpp
@@ -24,8 +24,41 @@ using std::streamsize;
 using std::vector;
 using std::string;
 
+
+int calculate_pairs(vector<int> vec) {
+	int result{};
+	
+	// nested for loop here
+	for (size_t i = 0; i < vec.size(); ++i) {
+
+		cout << "\n--> Outer iteration: " << (i+1) << endl;
+
+
+		for (size_t j = i + 1; j < vec.size(); ++j) {
+
+			cout << "\n\t--> Current: " << vec.at(i) << " * " << vec.at(j) << " = " << (vec.at(i) * vec.at(j)) << endl;
+
+
+			result = result + vec.at(i) * vec.at(j);
+		}
+	}
+
+	return result;
+}
+
+
 int main()
 {
-    cout << "Nested Loops - Sum of Product of all pairs of vector elements \n"; 
+		cout << "Nested Loops - Sum of Product of all pairs of vector elements \n"; 
+
+
+		//vector<int> vec1 = {1,2,3};
+		vector<int> vec1 = {2,4,6,8};
+
+		int result = calculate_pairs(vec1);
+		cout << "\n--> Result: " << result << endl;
+
+
+
 }
 
