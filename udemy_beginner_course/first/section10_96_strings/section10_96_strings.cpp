64e45dfead20a918ef8ddd2f66458cf778df646e
Mats Wikmar <matwik@gmail.com>
2018-12-31 15:53:27 +0100

    section 0_96 strings

---
+++ udemy_beginner_course/first/section10_96_strings/section10_96_strings.cpp
@@ -0,0 +1,34 @@
+
+#include "pch.h"
+#include <iostream>
+
+#include <limits>
+#include <climits>
+#include <cfloat>
+
+#include <vector>
+#include <string>
+#include <locale>     
+#include <set> 
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::dec;
+using std::hex;
+using std::hexfloat;
+using std::defaultfloat;
+using std::numeric_limits;
+using std::showbase;
+using std::streamsize;
+
+using std::vector;
+using std::string;
+using std::locale;
+using std::set;
+
+int main()
+{
+	cout << "\n\nSection 10 96 - strings \n" << endl;
+
+}
