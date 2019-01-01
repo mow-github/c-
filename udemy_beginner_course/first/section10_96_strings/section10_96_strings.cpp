71e812f46c61de52655bc5f080ceaadab07289e2
Mats Wikmar <matwik@gmail.com>
2019-01-01 18:06:04 +0100

    section10_99 encrypt decrypt strings

--- udemy_beginner_course/first/section10_96_strings/section10_96_strings.cpp
+++ udemy_beginner_course/first/section10_96_strings/section10_96_strings.cpp
@@ -11,6 +11,9 @@
 #include <locale>     
 #include <set> 
 
+#include <cstring>  // for c-style string functions
+#include <cctype>   // for character based functions
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -30,5 +33,37 @@ using std::set;
 int main()
 {
 	cout << "\n\nSection 10 96 - strings \n" << endl;
+
+
+	char first_name[20]{};
+	char last_name[20]{};
+	char full_name[50]{};
+	char temp[50]{};
+
+	cout << "Please enter your first name: " << endl;
+	cin >> first_name;
+
+	cout << "Please enter your last name: " << endl;
+	cin >> last_name;
+
+	cout << "-----------------------------" << endl;
+
+	cout << "Hello, " << first_name << " your first name has " << strlen(first_name) << " characters" << endl;
+
+	//strlen
+	//strcpy
+	//strcat
+	//strcmp
+	
+	// size_t			// instead of etc int i = 0 in a for-loop ( instead of unsigned int )
+
+	// isalpha
+	// toupper
+	// tolower
+
+
+	// cin.getline(full_name, 50);	// max 50 chars
+
+
 
 }
