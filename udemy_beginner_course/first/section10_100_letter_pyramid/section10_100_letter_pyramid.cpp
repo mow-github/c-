726864611d87ded074b63213c073f22bf755293b
Mats Wikmar <matwik@gmail.com>
2019-01-01 21:09:16 +0100

    section 10_100 challenge pyramid letter

--- udemy_beginner_course/first/section10_100_letter_pyramid/section10_100_letter_pyramid.cpp
+++ udemy_beginner_course/first/section10_100_letter_pyramid/section10_100_letter_pyramid.cpp
@@ -32,6 +32,62 @@ using std::set;
 
 int main()
 {
-    cout << "\n Section 10 100 - letter pyramid \n"; 
+
+
+		cout << "\n Section 10 100 - letter pyramid \n"; 
+		cout << "\n\n\n\n" << endl;
+
+
+		string userChars{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+
+		// space		A		space
+		// 	A				B		A			
+
+		//string userChars{ "ABC" };
+		//
+		// space	space		A		space space
+		// space	A				B		A			space
+		// A			B				C		B			A
+
+		// Ref: https://www.programiz.com/cpp-programming/examples/pyramid-pattern
+
+		int rows, count = 0, count1 = 0, k = 0;
+
+		rows = userChars.length();
+
+		for (int i = 1; i <= rows; ++i)
+		{
+			for (int space = 1; space <= rows - i; ++space)
+			{
+				cout << "  ";
+				++count;
+			}
+
+			while (k != 2 * i - 1)
+			{
+				if (count <= rows - 1)
+				{
+					//cout << i + k << " ";
+					cout << (userChars[k]) << " ";
+					++count;
+				}
+				else
+				{
+					++count1;
+					//cout << i + k - 2 * count1 << " ";
+					cout << ( userChars[k-2 * count1] )  << " ";
+				}
+				++k;
+			}
+			count1 = count = k = 0;
+
+			cout << endl;
+		}
+
+
+
+		cout << "\n\n\n\n" << endl;
+
+
 }
 
