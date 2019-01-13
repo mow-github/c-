02041926b70a47c8e23da5ea7d392bbe4a726f33
Mats Wikmar <matwik@gmail.com>
2019-01-13 12:07:00 +0100

    sections 11  functions: pass by value vs pass by ref

--- udemy_beginner_course/first/section11_105/section11_105.cpp
+++ udemy_beginner_course/first/section11_105/section11_105.cpp
@@ -29,8 +29,39 @@ using std::string;
 using std::locale;
 using std::set;
 
+
+void pass_by_value(int num);
+void pass_by_ref(int &num);
+
+
+void pass_by_value(int num) {
+	num = 100;
+}
+
+void pass_by_ref(int &num) {
+	num = 100;
+}
+
 int main()
 {
-    cout << "section 11 - functions:" << endl; 
+    cout << "section 11 - functions:\n" << endl; 
+
+		int num{50};
+
+		// pass by value - should not change the ORG value
+		cout << "pass_by_value (BEFORE) num: "<< num << endl;
+		pass_by_value(num);
+		cout << "pass_by_value (AFTER) num: " << num << endl;
+
+		cout << "\n" << endl;
+
+		// pass by ref - should change the ORG value
+		cout << "pass_by_ref (BEFORE) num: " << num << endl;
+		pass_by_ref(num);
+		cout << "pass_by_ref (AFTER) num: " << num << endl;
+
+		cout << "\n" << endl;
+
+
 }
 
