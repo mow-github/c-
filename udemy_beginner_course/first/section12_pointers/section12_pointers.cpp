203241d3a51b2a2330abea19df6b5de1c1eb1854
Mats Wikmar <matwik@gmail.com>
2019-01-20 20:03:07 +0100

    section 12 pointers start

---
+++ udemy_beginner_course/first/section12_pointers/section12_pointers.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include <iostream>
+
+#include <limits>
+#include <climits>
+#include <cfloat>
+
+#include <vector>
+#include <string>
+#include <locale>     
+#include <set> 
+
+#include <cstring>  // for c-style string functions
+#include <cctype>   // for character based functions
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::dec;
+using std::hex;
+using std::hexfloat;
+using std::defaultfloat;
+using std::numeric_limits;
+using std::showbase;
+using std::streamsize;
+
+using std::vector;
+using std::string;
+using std::locale;
+using std::set;
+
+
+int main()
+{
+    std::cout << "-- Pointers start -- \n\n";
+
+}
+
