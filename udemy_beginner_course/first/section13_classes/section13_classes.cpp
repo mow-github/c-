6382eb47b844e431f85a5cfbb2b7bc3a7742280a
Mats Wikmar <matwik@gmail.com>
2019-01-27 16:37:00 +0100

    section13 - modify copy and move classes

--- udemy_beginner_course/first/section13_classes/section13_classes.cpp
+++ udemy_beginner_course/first/section13_classes/section13_classes.cpp
@@ -101,7 +101,8 @@ TestDeepCopyClass::TestDeepCopyClass(int d, string name) {
 	data = new int;
 	*data = d;
 	_name = name;
-	std::cout << "--> TestDeepCopyClass CTOR 1arg called \n";
+	//std::cout << "--> TestDeepCopyClass CTOR 1arg called, value: " << *data << endl;
+	printf("--> TestDeepCopyClass CTOR 2arg called, value: %35d, nameObj: %5s \n", *data, _name.c_str());
 }
 //TestDeepCopyClass::TestDeepCopyClass(const TestDeepCopyClass &source) {
 //	data = new int;
@@ -112,13 +113,16 @@ TestDeepCopyClass::TestDeepCopyClass(const TestDeepCopyClass &source)
 	: TestDeepCopyClass{ *source.data, source._name } {
 	// delegate creation to the CTOR 2arg 
 
-	std::cout << "--> TestDeepCopyClass COPY CTOR 1arg called: modified and send to CTOR \n";
+	//std::cout << "--> TestDeepCopyClass COPY CTOR 1arg called: modified and send to CTOR \n";
+	printf("--> TestDeepCopyClass COPY CTOR 1arg called: modified and send to CTOR: %10d, nameObj: %5s \n", *data, _name.c_str());
+
 }
 
 
 TestDeepCopyClass::~TestDeepCopyClass() {
+	//std::cout << "--> TestDeepCopyClass DTOR called: " << this->_name << endl;
+	printf("--> TestDeepCopyClass DTOR called, value: %40d, nameObj: %5s \n", *data, _name.c_str());
 	delete data;
-	std::cout << "--> TestDeepCopyClass DTOR called: " << this->_name << endl;
 }
 void TestDeepCopyClass::printData() {
 	std::cout << "printData: " << *data << endl;
@@ -136,27 +140,29 @@ private:
 	int *data;
 	string _name;
 public:
-	// CTOR 1arg
-	TestMoveCtorClass(int d) {
+	// CTOR 2arg
+	TestMoveCtorClass(int d, string name) {
 		_counterObjects++;
+
+		_name = name;
 
 		data = new int;
 		*data = d;
-		std::cout << "-- ctor 1arg: \t\t" << d << endl;
+		std::cout << "-- ctor 2arg: \t\t\t\t"  << d << "\tname: " << _name << endl;
 	};
 
 	// COPY CTOR 1arg
-	TestMoveCtorClass(const TestMoveCtorClass &source)
-		:TestMoveCtorClass{ *source.data } {
-		std::cout << "-- COPY ctor 1arg: \t" << *data << endl;
-	};
+	//TestMoveCtorClass(const TestMoveCtorClass &source)
+	//	:TestMoveCtorClass{ *source.data } {
+	//	std::cout << "-- COPY ctor 1arg: \t" << *data << endl;
+	//};
 
 	// MOVE CTOR 1arg - "steal the data and null out the source ptr"
-	//TestMoveCtorClass(TestMoveCtorClass &&source)
-	//	: data{ source.data } {
-	//	source.data = nullptr;
-	//	std::cout << "-- MOVE ctor 1arg: " << *data << endl;
-	//};
+	TestMoveCtorClass(TestMoveCtorClass &&source)
+		: data{ source.data }, _name{source._name} {
+		source.data = nullptr;
+		std::cout << "-- MOVE ctor 1arg: \t\t\t" << *data << "\tname: " << _name << endl;
+	};
 
 	static void get_counterObjects() {
 		cout << endl;
@@ -170,10 +176,10 @@ public:
 
 		if (data != nullptr)
 		{
-			std::cout << "-- dtor freeing data: \t" << *data << endl;
+			std::cout << "-- dtor freeing data: \t\t\t" << *data << "\tname: " << _name << endl;
 		}
 		else {
-			std::cout << "-- dtor freeing data for nullptr" << endl;
+			std::cout << "-- dtor freeing data for nullptr" << "\t\tname: " << _name << endl;
 		}
 		delete data;
 	};
@@ -228,39 +234,43 @@ int main()
 
 
 
-	{
+	//{
 		// Problem1 - shallowCopy: only the pointer is copied and not what is's pointing to
-		std::cout << "\n ---------------------------------------------------------------------- \n" << endl;
-		std::cout << "\n Copy constructor - shallow: START \n" << endl;
+	//	std::cout << "\n ---------------------------------------------------------------------- \n" << endl;
+	//	std::cout << "\n Copy constructor - shallow: START \n" << endl;
 
-		TestShallowCopyClass testShallowCopyClassObj1(5, "obj1");
-		TestShallowCopyClass testShallowCopyClassObj2(5, "obj2");
-		//TestShallowCopyClass testShallowCopyClassObj2(testShallowCopyClassObj1);
-		std::cout << "\n Copy constructor - shallow: END \n" << endl;
-		std::cout << "\n ---------------------------------------------------------------------- \n" << endl;
+	//	TestShallowCopyClass testShallowCopyClassObj1(5, "obj1");
+	//	TestShallowCopyClass testShallowCopyClassObj2(5, "obj2");
+	//	//TestShallowCopyClass testShallowCopyClassObj2(testShallowCopyClassObj1);
+	//	std::cout << "\n Copy constructor - shallow: END \n" << endl;
+	//	std::cout << "\n ---------------------------------------------------------------------- \n" << endl;
 
-	}
+	//}
 
 	{
 		std::cout << "\n ---------------------------------------------------------------------- \n" << endl;
 		std::cout << "\n Copy constructor - DEEP copy: START \n" << endl;
 
-		TestDeepCopyClass testDeepCopyClassObj1(7, "obj1");
-		testDeepCopyClassObj1.printData();
-		TestDeepCopyClass testDeepCopyClassObj2(testDeepCopyClassObj1);
-		testDeepCopyClassObj2.printData();
+		vector<TestDeepCopyClass> vecDeepCopy;
+		vecDeepCopy.push_back(TestDeepCopyClass{10, "obj1"});
+		vecDeepCopy.push_back(TestDeepCopyClass{20, "obj2" });
 
-		std::cout << "\n --- deep copy: change obj1 --- \n" << endl;
-		// test: verify that obj1 changes data and obj2 data is the same as before
-		testDeepCopyClassObj1.setData(555);
-		testDeepCopyClassObj1.printData();
-		testDeepCopyClassObj2.printData();
+		//TestDeepCopyClass testDeepCopyClassObj1(7, "obj1");
+		//testDeepCopyClassObj1.printData();
+		//TestDeepCopyClass testDeepCopyClassObj2(testDeepCopyClassObj1);
+		//testDeepCopyClassObj2.printData();
 
-		std::cout << "\n --- deep copy: change obj2 --- \n" << endl;
-		// test: verify that obj2 changes data and obj1 data is the same as before
-		testDeepCopyClassObj2.setData(23);
-		testDeepCopyClassObj2.printData();
-		testDeepCopyClassObj1.printData();
+		//std::cout << "\n --- deep copy: change obj1 --- \n" << endl;
+		//// test: verify that obj1 changes data and obj2 data is the same as before
+		//testDeepCopyClassObj1.setData(555);
+		//testDeepCopyClassObj1.printData();
+		//testDeepCopyClassObj2.printData();
+
+		//std::cout << "\n --- deep copy: change obj2 --- \n" << endl;
+		//// test: verify that obj2 changes data and obj1 data is the same as before
+		//testDeepCopyClassObj2.setData(23);
+		//testDeepCopyClassObj2.printData();
+		//testDeepCopyClassObj1.printData();
 
 		std::cout << "\n Copy constructor - DEEP copy: END \n" << endl;
 		std::cout << "\n ---------------------------------------------------------------------- \n" << endl;
@@ -271,12 +281,12 @@ int main()
 		std::cout << "\n MOVE constructor: START \n" << endl;
 
 		vector<TestMoveCtorClass> vec;
-		vec.push_back(TestMoveCtorClass{10});
-		vec.push_back(TestMoveCtorClass{20});
+		vec.push_back(TestMoveCtorClass{10, "moveObj1"});
+		vec.push_back(TestMoveCtorClass{20, "moveObj2" });
 
 		// --------------------------- use static counter and output x TestMoveCtorClass objects
 		// Note: get_counterObjects should be 0 when we leave this scope.
-		TestMoveCtorClass::get_counterObjects();
+		//TestMoveCtorClass::get_counterObjects();
 		// ---------------------------
 
 		std::cout << "\n MOVE constructor: END \n" << endl;
