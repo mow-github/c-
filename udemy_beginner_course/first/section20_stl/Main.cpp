d88fc4ba2d32a8ae98decd127393a1be0dfe0711
Mats Wikmar <matwik@gmail.com>
2019-04-08 22:54:53 +0200

    stl

--- udemy_beginner_course/first/section20_stl/Main.cpp
+++ udemy_beginner_course/first/section20_stl/Main.cpp
@@ -33,11 +33,38 @@ using std::set;
 
 
 
+struct Person {
+	string name;
+	int age;
+
+	bool operator >(const Person &rhs) const{
+		return this->age > rhs.age;
+	}
+
+};
+
+
+template<typename T>
+
+T max(T a, T b){
+	return (a > b) ? a : b;
+}
+
+
 int main()
 {
-	cout << "stl start";
+	cout << "stl start" << endl;
 	
+	auto result = max(2, 5);
 
+	cout << "result: " << result << endl;
+
+
+
+	Person p1{"Adam", 23};
+	Person p2{"Bert", 33};
+	Person p3 = max(p1,p2);
+	cout << "result: " << p3.name << endl;
 
 
 
